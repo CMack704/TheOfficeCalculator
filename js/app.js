4277347e7ae0d2cee8637c5a0a6813a9b8e68b72
Craig McAlister <craigmack704@gmail.com>
2021-03-07 22:43:46 -0500

    javascript to make calculator work

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// function to get values and print on screen
+function getValue() {
+    let input = event.target.innerText;
+    console.log(input);
+    printValue(input);
+};
+
+function printValue(val) {
+    // variables to grab result id in html and change html
+    let output = document.querySelector('#result');
+    let current = output.innerHTML;
+    // if/else to change html to desired output in html
+    if(output.innerHTML == '0') {
+        if(val != 'C' && val != 'DEL'){
+            output.innerHTML = '';
+            output.innerHTML += val;
+        };
+
+    } else{
+        if( val == 'DEL'){
+            output.innerHTML = current.slice(0, -1);
+            if( output.innerHTML.length <= 1){
+                output.innerHTML = '0';
+            };
+        };
+        if( val != 'C' && val != 'DEL' && val != '='){
+            output.innerHTML += val;
+        };
+        if( val == 'C'){
+            output.innerHTML = '0';
+        };
+        if( val == '='){
+            let result = output.innerHTML;
+            output.innerHTML = eval(result);// use eval function to make calculations
+        };
+
+    };
+};
+// grabs button values
+let buttons = document.getElementsByTagName('button');
+for(let i = 0; i<buttons.length; i++) {
+    buttons[i].setAttribute('onclick', 'getValue()');
+};
